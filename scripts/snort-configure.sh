835504fe26c439daba5ced9c8a96881b32b7f685
Waymouth <swaym@amazon.co.uk>
2019-11-26 10:40:30 +0000

    updated the snort-configure.sh script

--- scripts/snort-configure.sh
+++ scripts/snort-configure.sh
@@ -3,10 +3,10 @@ cd ~
 sudo yum install git
 git clone https://github.com/waymousa/aws-reinvent-2019-builders-session-opn215.git
 cd aws-reinvent-2019-builders-session-opn215
-sudo cp ../etc/aws-kinesis/agent.json /etc/aws-kinesis/agent.json
-sudo cp ../etc/rc.d/init.d/snortd /etc/rc.d/init.d/snortd
-sudo cp ../etc/snort/snort.conf /etc/snort/snort.conf
-sudo cp ../etc/sysconfig/snort /etc/sysconfig/snort
+sudo cp etc/aws-kinesis/agent.json /etc/aws-kinesis/agent.json
+sudo cp etc/rc.d/init.d/snortd /etc/rc.d/init.d/snortd
+sudo cp etc/snort/snort.conf /etc/snort/snort.conf
+sudo cp etc/sysconfig/snort /etc/sysconfig/snort
 sudo touch /etc/snort/rules/white_list.rules
 sudo touch /etc/snort/rules/black_list.rules
 sudo touch /etc/snort/rules/local.rules
