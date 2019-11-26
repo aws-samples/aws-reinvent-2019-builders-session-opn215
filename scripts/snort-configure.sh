b71559b298be8d5674c9cb98c0423205ab0599af
Waymouth <swaym@amazon.co.uk>
2019-11-26 11:06:29 +0000

    Updated the snort-configure script to deploy a default local rules file.

--- scripts/snort-configure.sh
+++ scripts/snort-configure.sh
@@ -7,9 +7,9 @@ sudo cp etc/aws-kinesis/agent.json /etc/aws-kinesis/agent.json
 sudo cp etc/rc.d/init.d/snortd /etc/rc.d/init.d/snortd
 sudo cp etc/snort/snort.conf /etc/snort/snort.conf
 sudo cp etc/sysconfig/snort /etc/sysconfig/snort
+sudo cp etc/snort/rules/local.rules /etc/snort/rules
 sudo touch /etc/snort/rules/white_list.rules
 sudo touch /etc/snort/rules/black_list.rules
-sudo touch /etc/snort/rules/local.rules
 sudo mkdir /usr/local/lib/snort_dynamicrules
 sudo chown -R snort:snort /usr/local/lib/snort_dynamicrules
 sudo chmod 2775 /var/log/snort
