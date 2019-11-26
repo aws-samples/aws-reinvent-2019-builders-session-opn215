2bb48f7cf47bc93008c7c21b677a9ca90be33a5f
Waymouth <swaym@amazon.co.uk>
2019-11-26 10:32:34 +0000

    updated the scripts to fix various bugs

--- scripts/snort-configure.sh
+++ scripts/snort-configure.sh
@@ -7,3 +7,8 @@ sudo cp ../etc/aws-kinesis/agent.json /etc/aws-kinesis/agent.json
 sudo cp ../etc/rc.d/init.d/snortd /etc/rc.d/init.d/snortd
 sudo cp ../etc/snort/snort.conf /etc/snort/snort.conf
 sudo cp ../etc/sysconfig/snort /etc/sysconfig/snort
+sudo touch /etc/snort/rules/white_list.rules
+sudo touch /etc/snort/rules/black_list.rules
+sudo touch /etc/snort/rules/local.rules
+sudo mkdir /usr/local/lib/snort_dynamicrules
+sudo chown -R snort:snort /usr/local/lib/snort_dynamicrules
--- scripts/snort-install.sh
+++ scripts/snort-install.sh
@@ -6,13 +6,13 @@ echo "INFO: Adding EPEL repo"
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sleep 1
 echo "INFO: Installing Kinesis agent"
-sudo yum localinstall –y https://s3.amazonaws.com/streaming-data-agent/aws-kinesis-agent-latest.amzn1.noarch.rpm
+sudo yum install –y https://s3.amazonaws.com/streaming-data-agent/aws-kinesis-agent-latest.amzn1.noarch.rpm
 sleep 1
 echo "INFO: Installing daq package"
-sudo yum localinstall -y https://www.snort.org/downloads/archive/snort/daq-2.0.6-1.f21.x86_64.rpm
+sudo yum install -y https://www.snort.org/downloads/archive/snort/daq-2.0.6-1.f21.x86_64.rpm
 sleep 1
 echo "INFO: Installing snort package"
-sudo yum localinstall -y https://www.snort.org/downloads/archive/snort/snort-openappid-2.9.9.0-1.f21.x86_64.rpm
+sudo yum install -y https://www.snort.org/downloads/archive/snort/snort-openappid-2.9.9.0-1.f21.x86_64.rpm
 sleep 1
 echo "INFO: Installing snort community rules"
 sudo wget -nv https://www.snort.org/downloads/community/community-rules.tar.gz -O /var/tmp/community-rules.tar.gz
